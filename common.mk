bb30007505df52f793a394eae8f7de92957c38eb
Namkhai B <me@forkbomb9.ch>
2023-09-02 16:34:58 -0600

    msm89988-common: Retrofit Dynamic Partitions
    This is a squash of the following commits:
    
    * Specify a non-existent dt directory to avoid using dt fstab
    * Add system_ext, product and odm partitions
    * drop support for vboot 1.0
    * Flash dummy super image if needed
    * AndroidBoard: Fix image paths
    * Move super_dummy build to Android.mk
    
    Co-authored-by: me-cafebabe <me_cafebabe@hotmail.com>
    Co-authored-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Co-authored-by: P-Salik <peersalik16@gmail.com>
    Co-authored-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Namkhai B <me@forkbomb9.ch>

--- common.mk
+++ common.mk
@@ -155,6 +155,14 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
@@ -209,6 +217,7 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
+    fstab.qcom_ramdisk \
     fstab.qcom \
     init.devstart.sh \
     init.qcom.rc \
@@ -401,6 +410,8 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.0-service.basic
+
 
 # Vibrator
 PRODUCT_PACKAGES += \
